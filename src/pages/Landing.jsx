5a7629ed5845974147933e7171f47d92840eccbd
Shree Dhamija <shreedhamija@gmail.com>
2025-07-02 13:43:15 +0530

    landing page email

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -374,7 +374,7 @@ const Landing = () => {
                             <img src={logoImg || "/placeholder.svg"} alt="Blip logo" className={styles.footerLogo} />
                             <span className={styles.footerBrandText}>Blip</span>
                         </div>
-                        <div className={styles.footerEmail}><a href="mailto:hello@storeos.co"> hello@storeos.co</a></div>
+                        <div className={styles.footerEmail}><a href="mailto:shree@withblip.com"> shree@withblip.com</a></div>
                     </div>
                     <div className={styles.footerRight}>
                         <a href="/privacy-policy" target="_blank" className={styles.footerLink}>
