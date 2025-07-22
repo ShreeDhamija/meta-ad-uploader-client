3596cab97c463639d78c1c8dc4a58f86484c205a
Shree Dhamija <shreedhamija@gmail.com>
2025-07-22 13:46:50 +0530

    add duplicated aset v2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 // "use client"
 
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useCallback } from "react"
 import { toast, Toaster } from "sonner"
 import { useNavigate } from "react-router-dom"
 
