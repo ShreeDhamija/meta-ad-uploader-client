27ef9c2cb9a513aa8b3e990c857136d1dad468db
Shree Dhamija <shreedhamija@gmail.com>
2025-10-17 14:42:08 +0530

    team member plan sync

--- src/lib/useSubscriptionSettings.js
+++ src/lib/useSubscriptionSettings.js
@@ -49,9 +49,9 @@ export default function useSubscription() {
             if (now > cancelDate) return false;
         }
 
-        if (subscriptionData.teamId && !subscriptionData.isTeamOwner) {
-            return true;
-        }
+        // if (subscriptionData.teamId && !subscriptionData.isTeamOwner) {
+        //     return true;
+        // }
 
 
         // Existing logic
