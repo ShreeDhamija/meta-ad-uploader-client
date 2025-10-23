2c6f8940292254aeee44b9aba183ac87edba8b69
Shree Dhamija <shreedhamija@gmail.com>
2025-10-23 15:32:58 +0530

    conditional flex ad display 2

--- src/components/ad-creation-form.jsx
+++ src/components/ad-creation-form.jsx
@@ -2719,17 +2719,17 @@ export default function AdCreationForm({
               <SelectTrigger className="w-[180px] bg-white border-gray-400 rounded-xl font-medium">
                 <SelectValue placeholder="Select ad type" />
               </SelectTrigger>
-              <SelectContent className="bg-white rounded-xl space-y-2" >
+              <SelectContent className="bg-white rounded-xl gap-4" >
                 <SelectItem
                   value="regular"
-                  className="rounded-xl data-[highlighted]:bg-gray-50 data-[state=checked]:bg-gray-100 transition-all"
+                  className="rounded-xl data-[highlighted]:bg-gray-100 data-[state=checked]:bg-gray-100 transition-all my-0.5"
                 >
                   Image / Video
                 </SelectItem>
 
                 <SelectItem
                   value="carousel"
-                  className="rounded-xl data-[highlighted]:bg-gray-100 data-[state=checked]:bg-gray-100 transition-all"
+                  className="rounded-xl data-[highlighted]:bg-gray-100 data-[state=checked]:bg-gray-100 transition-all my-0.5"
                 >
                   Carousel
                 </SelectItem>
@@ -2737,7 +2737,7 @@ export default function AdCreationForm({
                 {["OUTCOME_SALES", "OUTCOME_APP_PROMOTION"].includes(campaignObjective) && (
                   <SelectItem
                     value="flexible"
-                    className="rounded-xl data-[highlighted]:bg-gray-100 data-[state=checked]:bg-gray-100 transition-all"
+                    className="rounded-xl data-[highlighted]:bg-gray-100 data-[state=checked]:bg-gray-100 transition-all my-0.5"
                   >
                     Flexible Ads
                   </SelectItem>
