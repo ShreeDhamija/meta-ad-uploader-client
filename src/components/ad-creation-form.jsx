613e68e1ec0e71ac90aa9280e751035cd0453fbf
Shree Dhamija <shreedhamija@gmail.com>
2026-07-13 14:30:39 +0530

    csv 3.2

--- src/components/ad-creation-form.jsx
+++ src/components/ad-creation-form.jsx
@@ -9888,9 +9888,12 @@ export default function AdCreationForm({
             <div className="mt-4 text-left text-sm leading-6 text-gray-700">
               <ul className="list-disc space-y-2 pl-5">
                 <li>Each row becomes a new form variant.</li>
-                <li>Columns Supported: Campaign Name, Ad Set Name, Ad Name, Facebook Page, URL, Google Drive Link, Primary Text and Headlines 1 through 5.</li>
+                <li>
+                  Columns Supported: <strong>Campaign Name</strong>, <strong>Ad Set Name</strong>, <strong>Ad Name</strong>, <strong>Facebook Page</strong>, <strong>URL</strong>, <strong>Google Drive Link</strong>, <strong>Primary Text</strong>, <strong>Headlines</strong> and <strong>Descriptions</strong> 1 through 5.
+                </li>
                 <li>You can leave out Ad name and Facebook page if setup in preferences.</li>
-                <li>You can locally upload creatives and assign them to variants as well.</li>
+                <li>If you're not using google drive then you can leave out that column and manually upload creatives and assign them to variants as well.</li>
+                <li>Only single image/video ad type is currently supported.</li>
               </ul>
             </div>
 
